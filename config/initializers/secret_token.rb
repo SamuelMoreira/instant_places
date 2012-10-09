0e570defd05b0c0a12e7b272c9d414e55d14b44e
samuel <genesis.samuel@gmail.com>
2012-10-09 16:22:10 +0100

    first commitfirst commitfirst commitfirst commitfirst commitfirst commitfirst commitfirst commitfirst commitfirst commitfirst commitfirst commitfirst commitfirst commitfirst commitfirst commitfirst commitfirst commitfirst commitfirst commitfirst commitfirst commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SLIp::Application.config.secret_token = 'b6bf69a44f597c7faba9065e3c46b27dc1a4727882c3aec6a5ec1429e3734a4b8c8f07873a2532129d2d3123745c1cb62648a109f634a8f036dabf715da158eb'
